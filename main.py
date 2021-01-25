0d27648292f3ba538c7d828378967a23ebd88924
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-25 07:52:19 +0000

    Refactor the code

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ from side import alphabetNumeric, logo
 print(logo)
 print('IMPORTANT:\nThe shift number is vital to encryption and decryption process.\n'
       'The shift number must be between 1 and 20.\n'
-      'Keep the shift number safe and separated from the result.\n')
+      'Keep the shift number safe and separated from the result.')
 
-# User prompts
-purpose = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input(f"Text to {purpose}:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # The encryption and decryption process
-
-
 def cipher(initial_text, shift_num, cipher_purpose):
     cipher_text = ""
     if cipher_purpose == "decode":
@@ -32,13 +26,29 @@ def cipher(initial_text, shift_num, cipher_purpose):
     print(f"Here's the {purpose}d result: {cipher_text}")
 
 
-# Validating user inputs
-if (purpose == 'encode') or (purpose == 'decode'):
-    # Prevent IndexError
-    if (shift < 1) or (shift > 20):
-        print('Invalid shift number')
-    else:
-        cipher(initial_text=text, shift_num=shift, cipher_purpose=purpose)
-else:
-    print('Invalid response. Encode or Decode')
+cipher_continue = True
+while cipher_continue:
+    # User prompts
+    purpose = input("\nDo you want to encrypt? type 'encode' otherwise 'decode' to decrypt:\n").lower()
+    text = input(f"Text to {purpose}:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
+    # Validating user inputs
+    if (purpose == 'encode') or (purpose == 'decode'):
+        # Prevent IndexError
+        if (shift < 1) or (shift > 20):
+            print('Invalid shift number')
+        else:
+            cipher(initial_text=text, shift_num=shift, cipher_purpose=purpose)
+    else:
+        print('Invalid response. Encode or Decode')
+
+    restart = input("\nType 'yes' to restart or 'no' to close the program.\n").lower()
+    if restart == "yes":
+        cipher_continue = True
+    elif restart == "no":
+        cipher_continue = False
+        print("Thank you for using the application.")
+    else:
+        print('Invalid Response, Restarting the application.')
+        cipher_continue = True
