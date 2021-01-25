8d04b6e2e551861ea601535e613c74024659b2ef
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-25 07:29:48 +0000

    Add if statement to the cipher function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """
 This application encrypts and decrypt messages/text according to the shift key
 """
-from side import alphabets
+from side import alphabetNumeric, logo
 
 # Default displays
-print('The text are converted into lower case, be careful not too make a mistake.')
-print('The shift number must be between 1 and 20.\n')
+print(logo)
+print('IMPORTANT:\nThe shift number is vital to encryption and decryption process.\n'
+      'The shift number must be between 1 and 20.\n'
+      'Keep the shift number safe and separated from the result.\n')
 
 # User prompts
 purpose = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
@@ -20,9 +22,13 @@ def cipher(initial_text, shift_num, cipher_purpose):
     if cipher_purpose == "decode":
         shift_num *= -1
     for letter in initial_text:
-        position = alphabets.index(letter)
-        new_position = position + shift_num
-        cipher_text += alphabets[new_position]
+        # To keep the inputs not in the list
+        if letter in alphabetNumeric:
+            position = alphabetNumeric.index(letter)
+            new_position = position + shift_num
+            cipher_text += alphabetNumeric[new_position]
+        else:
+            cipher_text += letter
     print(f"Here's the {purpose}d result: {cipher_text}")
 
 
