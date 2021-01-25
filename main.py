7d250ee9c9338df0663a57438a6e6f67cbaa19e1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-25 06:24:40 +0000

    Add text decryption function

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ This application encrypts and decrypt messages/text according to the shift key
 from side import alphabets
 
 # Default displays
-print('The text are converted into lower case, be careful not too make a mistake.\n')
+print('The text are converted into lower case, be careful not too make a mistake.')
+print('The shift number must be between 1 and 20.\n')
 
 # User prompts
+purpose = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Text to encrypt:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 
 # Text Encryption
 
@@ -23,5 +24,29 @@ def encrypt(initial_text, shift_num):
         encoding += new_letter
     print(f'The encoded text is: {encoding}')
 
+# Text Decryption
 
-encrypt(text, shift)
+
+def decrypt(encrypt_text, shift_num):
+    initial_text = ""
+    for letter in encrypt_text:
+        position = alphabets.index(letter)
+        old_position = position - shift_num
+        initial_text += alphabets[old_position]
+    print(f"The decoded text is {initial_text}")
+
+
+if purpose == 'encode':
+    # Prevent IndexError
+    if (shift < 1) or (shift > 20):
+        print('Invalid shift number')
+    else:
+        encrypt(initial_text=text, shift_num=shift)
+elif purpose == 'decode':
+    # Prevent IndexError
+    if (shift < 1) or (shift > 20):
+        print('Invalid shift number')
+    else:
+        decrypt(encrypt_text=text, shift_num=shift)
+else:
+    print('Invalid response. Encode or Decode')
